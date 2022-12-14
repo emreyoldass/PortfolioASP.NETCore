4ba7e7b758582d7f36205b6ded1235073ed665d4
emre yoldas <eyoldas7@gmail.com>
2022-12-14 23:30:59 +0300

    Experience and portfolio View page added and View Component added

---
+++ Portfolio.NETCore/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.NETCore.ViewComponents.Experience
+{
+    public class ExperienceList : ViewComponent
+    {
+        ExperienceManager experinceManager = new ExperienceManager(new EfExperienceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = experinceManager.TGetList();
+            return View(values);
+        }
+    }
+}
+
