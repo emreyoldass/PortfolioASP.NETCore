b455cb3efbb07e9f41ce6566feabe19ea93bd3bf
emre yoldas <eyoldas7@gmail.com>
2022-12-20 16:05:59 +0300

    Admin controller, Admin view page, partial side bar and AdminLayout Added

---
+++ Portfolio.NETCore/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.NETCore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
