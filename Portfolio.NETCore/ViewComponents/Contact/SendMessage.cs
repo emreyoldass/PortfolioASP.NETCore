dbe0e5b077bbe4676786a4af1c85cd953e25a118
emre yoldas <eyoldas7@gmail.com>
2022-12-20 13:12:53 +0300

    Contact and Message(send) ViewComponents and View Page added

---
+++ Portfolio.NETCore/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.NETCore.ViewComponents.Contact
+{
+    public class SendMessage:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IViewComponentResult Invoke(Message m)
+        //{
+        //    m.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+        //    m.Status = true;
+        //    messageManager.TAdd(m);
+        //    return View();
+        //}
+    }
+}
