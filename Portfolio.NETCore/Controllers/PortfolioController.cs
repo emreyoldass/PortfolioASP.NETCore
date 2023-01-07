d29b933dd27bcc6ab1f491b16a5342441e1baafd
emre yoldas <eyoldas7@gmail.com>
2023-01-07 19:21:41 +0300

    Portfolio index and add page added. Portfolio controller added.

---
+++ Portfolio.NETCore/Controllers/PortfolioController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.NETCore.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Listesi";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Proje Ekleme";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(EntityLayer.Concrete.Portfolio portfolio)
+        {
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
+        }
+       
+    }
+}
