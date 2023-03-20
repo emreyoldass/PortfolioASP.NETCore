9ca854fa28db39ee66d7589e375bc04838b2228c
emre yoldas <eyoldas7@gmail.com>
2023-03-20 16:58:46 +0300

    Dashboard Controller and view page added

---
+++ Portfolio.NETCore/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.NETCore.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Dashboard";
+            ViewBag.v2 = "İstatistikler";
+            ViewBag.v3 = "İstatistikler Sayfası";
+            return View();
+        }
+    }
+}
